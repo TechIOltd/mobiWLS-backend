0d3a89fd4d486113f965065c24abdb250fedb5d2
Filip Slavik <filip@techio.com>
2013-06-13 00:10:06 +0300

    updated license information on source files

---
+++ mod-web/src/main/java/com/techio/mobiwls/datasets/MetricDataSet.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2013 TechIO Ltd (http://techio.com)
+ * 
+ * (http://techio.com/portfolio/mobile-applications)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.techio.mobiwls.datasets;
+
+import java.util.Date;
+import java.util.concurrent.ArrayBlockingQueue;
+
+/**
+ * @author <a href="mailto:filip@techio.com">Filip Slavik</a>
+ *
+ */
+public class MetricDataSet {
+
+private ArrayBlockingQueue<MetricSample> samples;
+	
+	private Date youngestEntry = null;
+	public MetricDataSet() {
+		
+	}
+	
+	public void addSample(MetricSample sample) {
+		try {
+			if (samples.remainingCapacity() == 0) {
+				/* no more space in queue, remove the item currently on queue's head */
+				MetricSample element = samples.take();
+			}
+			// add the new sample and update the youngest entry date
+			samples.add(sample);
+			this.youngestEntry = sample.getSampledOn();
+		} catch (Exception ex) {
+			throw new RuntimeException(ex);
+		}
+	}
+
+	
+
+	public ArrayBlockingQueue<MetricSample> getSamples() {
+		return samples;
+	}
+
+
+	
+	public Date getOldestEntry() {
+		MetricSample oldestEntry = samples.peek();
+		return oldestEntry != null ? oldestEntry.getSampledOn() : null;
+	}
+
+	public Date getYoungestEntry() {
+		return youngestEntry;
+	}
+
+	public Integer getEntriesCount() {
+		return samples != null ? samples.size() : 0;
+	}
+
+}
--- mod-web/src/main/java/com/techio/mobiwls/rest/resources/ServerResource.java
+++ mod-web/src/main/java/com/techio/mobiwls/rest/resources/ServerResource.java
@@ -1,6 +1,21 @@
-/**
+/*
+ * Copyright (c) 2013 TechIO Ltd (http://techio.com)
  * 
+ * (http://techio.com/portfolio/mobile-applications)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 package com.techio.mobiwls.rest.resources;
 
 import java.util.ArrayList;
@@ -38,7 +53,7 @@ import commonj.timers.TimerListener;
 import commonj.timers.TimerManager;
 
 /**
- * @author slavikos
+ * @author <a href="mailto:filip@techio.com">Filip Slavik</a>
  * 
  */
 @Path("/server")
